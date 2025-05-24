d545d3122c349013be147049aad170ab0571a2e7
nov144 <nov144@mail.ru>
2025-05-25 01:42:13 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import os
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.reply("Привет! Я бот!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
