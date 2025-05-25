0ba40bf8e788b6e5ef8a44c12af9f8b23b13e1d4
nov144 <nov144@mail.ru>
2025-05-25 23:10:00 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+
+from states import BookingStates
 import os
 
-bot = Bot(token=os.getenv("BOT_TOKEN"))
-dp = Dispatcher(bot)
+# Инициализация бота
+bot = Bot(token=os.getenv("BOT_TOKEN"))  # BOT_TOKEN из Render переменной
+storage = MemoryStorage()  # FSM-хранилище
+dp = Dispatcher(bot, storage=storage)
 
+# Обработка /start
 @dp.message_handler(commands=["start"])
-async def start(message:types.Message):
-    await message.reply("Привет! Я бот!")
-@dp.message_handler(commands=["puk"])
-async def echo(message: types.Message):
-    await message.reply(f"Ты сказал: {message.text}")
+async def start_handler(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton("Записаться"))
+    keyboard.add(KeyboardButton("Контакты"))
+    await message.answer("Привет! Я бот для записи к мастеру. Выберите действие:", reply_markup=keyboard)
 
+# Нажатие на кнопку "Записаться"
+@dp.message_handler(lambda message: message.text == "Записаться")
+async def start_booking(message: types.Message):
+    await message.answer("Как вас зовут?")
+    await BookingStates.waiting_for_name.set()  # переключаемся в состояние "ждём имя"
+
+# Запуск бота
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
